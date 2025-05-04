c527bc3206ff485c95d0c6b7a02102f362c88c8a
akwasniewski <andrzej@akwasniewski.eu>
2025-05-04 22:20:50 +0200

    PN

--- build-one-docker.sh
+++ build-one-docker.sh
@@ -3,4 +3,3 @@ mkdir build/${1}
 TEXINPUTS="/data/common:chapters:$TEXINPUTS"
 CMD="pdflatex -interaction=nonstopmode -file-line-error -shell-escape --output-directory=../build/${1} main.tex"
 ./run-in-docker.sh /bin/bash -c "export TEXINPUTS=\"${TEXINPUTS}\"; cd ${1}; ${CMD}; ${CMD}"
-
